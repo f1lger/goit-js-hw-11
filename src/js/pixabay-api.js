bb2b336ad68d91ad6c29e08213cb0b2792d0c1a4
f1lger <palcinskijvolodimir173@gmail.com>
2024-04-09 09:44:10 +0300

    fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -12,7 +12,10 @@ export function fetchCards(searchQuery) {
 
   return fetch(`${BASE_URL}${SEARCH_PARAMS}`)
     .then(res => {
-    if (!res.ok) throw new Error('Failed to fetch');
-    return res.json();
-  });
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+      return res.json();
+    })
+    .catch(error => console.log(error));
 }
